d8456d918b8b01fc836b768efc8e2bbdfb23901a
Lynxy <the_lynxy@hotmail.com>
2010-10-13 12:03:45 -0400

    Updated ClientHandler

--- VectorNetServer/Classes/ClientHandler.cs
+++ VectorNetServer/Classes/ClientHandler.cs
@@ -9,11 +9,14 @@ namespace VectorNetServer
 {
     static public class ClientHandler
     {
+        public delegate void UserPacketReceivedDelegate(User user, byte[] data);
+        static public event UserPacketReceivedDelegate UserPacketReceived;
+
         static public Dictionary<TcpClientWrapper, User> Users = new Dictionary<TcpClientWrapper, User>();
 
         static public void AddNewClient(TcpClientWrapper client)
         {
-            Users.Add(client, new User());
+            Users.Add(client, new User(client));
             client.DataRead += new TcpClientWrapper.DataReadDelegate(client_DataRead);
             client.AsyncRead(1024, true);
         }
@@ -21,15 +24,15 @@ namespace VectorNetServer
         static private void client_DataRead(TcpClientWrapper sender, byte[] data)
         {
             User user = Users[sender];
-            int oldLen = user.packetBuffer.Length;
-            Array.Resize(ref user.packetBuffer, oldLen + data.Length);
-            Array.Copy(data, 0, user.packetBuffer, oldLen, data.Length);
+            int oldLen = user.Buffer.Length;
+            Array.Resize(ref user.Buffer, oldLen + data.Length);
+            Array.Copy(data, 0, user.Buffer, oldLen, data.Length);
 
             //check to see if whole packet is available
             byte[] completePacket;
-            while ((completePacket = PacketBuffer.GetNextPacket(ref user.packetBuffer)) != null)
+            while ((completePacket = PacketBuffer.GetNextPacket(ref user.Buffer)) != null)
             {
-                System.Windows.Forms.MessageBox.Show("Data recv: " + Encoding.ASCII.GetString(completePacket));
+                UserPacketReceived(user, completePacket);
             }
         }
     }
