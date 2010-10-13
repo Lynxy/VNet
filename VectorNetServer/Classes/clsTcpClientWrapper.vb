bb19451b2feec014eb22abb51191583518a7af25
Lynxy <the_lynxy@hotmail.com>
2010-10-13 01:03:08 -0400

    Update

--- VectorNetServer/Classes/clsTcpClientWrapper.vb
+++ VectorNetServer/Classes/clsTcpClientWrapper.vb
@@ -1,3 +1,14 @@
-﻿Public Class clsTcpClientWrapper
+﻿Imports System.Net
+Imports System.Net.Sockets
+Imports System.Threading
 
-End Class
+
+Namespace Lynxy.Network
+    Public Class TcpClientWrapper
+        Protected _base As TcpClient
+
+        Public Sub New(ByVal base As TcpClient)
+            _base = base
+        End Sub
+    End Class
+End Namespace
--- VectorNetServer/Forms/frmMain.vb
+++ VectorNetServer/Forms/frmMain.vb
@@ -1,4 +1,5 @@
-﻿Imports System.Net.Sockets
+﻿Imports System
+Imports System.Net.Sockets
 Imports VectorNetServer.Lynxy.Network
 Imports VectorNetServer.Lynxy.Security
 
@@ -7,24 +8,23 @@ Public Class frmMain
     Dim WithEvents listener As New TcpListenerWrapper(Me, 4800)
     Dim packet As New Packet()
 
-    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+    Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs) Handles MyBase.Load
         WriteINI("Main", "HostedBy", "Vector", "Config.ini")
 
-        'MsgBox("This is a test.")
         'MsgBox("This hoster is: " & ReadINI("Main", "HostedBy", "Config.ini"))
 
-        'This is a new comment
         'Call LoadConfig()
 
-
-        'socket.AsyncListen(10, "127.0.0.1", 4800)
         listener.Listen(10)
+
         Dim cli As New TcpClient()
         cli.Connect("127.0.0.1", 4800)
 
     End Sub
 
-    Private Sub socket2_OnClientConnected(ByVal newSock As System.Net.Sockets.TcpClient) Handles listener.OnClientConnected
-
+    Private Sub listener_OnClientConnected(ByVal newSock As TcpClient) Handles listener.OnClientConnected
+        Dim dat As Byte()
+        dat = packet.Clear().InsertString("test")
+        newSock.GetStream().BeginWrite(dat, 0, dat.Length, Nothing, Nothing)
     End Sub
 End Class
