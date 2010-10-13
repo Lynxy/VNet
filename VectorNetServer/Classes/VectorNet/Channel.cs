bf6a183b97875d3e474cb530454a87834a979af7
AlphaBlend <whizkid3000@hotmail.com>
2010-10-13 12:09:51 -0700

    Created two constructures, and the flag and channel name variables, for the Channel class

--- VectorNetServer/Classes/VectorNet/Channel.cs
+++ VectorNetServer/Classes/VectorNet/Channel.cs
@@ -7,5 +7,17 @@ namespace VectorNetServer
 {
     public class Channel
     {
+        private byte Flag;
+        private string name;
+
+        public Channel(string chan) : this(0x00, chan)
+        {
+        }
+
+        public Channel(byte f, string chan)
+        {
+            Flag = f;
+            name = chan;
+        }
     }
 }
