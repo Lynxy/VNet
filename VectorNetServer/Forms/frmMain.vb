676decf7a05919564cca9256c53ed996c0e36f1d
Lynxy <the_lynxy@hotmail.com>
2010-10-13 00:12:03 -0400

    Added clsTcpListenerWrapper and clsTcpClientWrapper

--- VectorNetServer/Forms/frmMain.vb
+++ VectorNetServer/Forms/frmMain.vb
@@ -4,30 +4,27 @@ Imports VectorNetServer.Lynxy.Security
 
 Public Class frmMain
     Dim cs As New CryptographicScheme(1024)
-    Dim WithEvents socket As New SocketWrapper(System.Net.Sockets.AddressFamily.InterNetwork, System.Net.Sockets.SocketType.Stream, System.Net.Sockets.ProtocolType.Tcp)
+    Dim WithEvents listener As New TcpListenerWrapper(Me, 4800)
     Dim packet As New Packet()
 
     Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
         WriteINI("Main", "HostedBy", "Vector", "Config.ini")
 
-        MsgBox("This is a test.")
-        MsgBox("This hoster is: " & ReadINI("Main", "HostedBy", "Config.ini"))
+        'MsgBox("This is a test.")
+        'MsgBox("This hoster is: " & ReadINI("Main", "HostedBy", "Config.ini"))
 
         'This is a new comment
         'Call LoadConfig()
 
-        socket.AsyncListen(10, "127.0.0.1", 4800)
-    End Sub
 
-    Private Sub socket_OnClientConnected(ByVal origSock As SocketWrapper, ByVal newSock As SocketWrapper) Handles socket.OnClientConnected
-        AddHandler newSock.OnDataReceived, AddressOf socket_OnDataReceived
-    End Sub
-
-    Private Sub socket_OnConnected(ByVal sock As SocketWrapper) Handles socket.OnConnected
+        'socket.AsyncListen(10, "127.0.0.1", 4800)
+        listener.Listen(10)
+        Dim cli As New TcpClient()
+        cli.Connect("127.0.0.1", 4800)
 
     End Sub
 
-    Private Sub socket_OnDataReceived(ByVal sock As SocketWrapper, ByVal data() As Byte) Handles socket.OnDataReceived
+    Private Sub socket2_OnClientConnected(ByVal newSock As System.Net.Sockets.TcpClient) Handles listener.OnClientConnected
 
     End Sub
 End Class
