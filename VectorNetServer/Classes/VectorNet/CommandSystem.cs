8a700a5f680996ec1fa0b2130f0d250d4c90b3a2
AlphaBlend <whizkid3000@hotmail.com>
2010-10-13 16:06:39 -0700

    Created CommandSystem.cs skeleton

---
+++ VectorNetServer/Classes/VectorNet/CommandSystem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VNET.Commands
+{
+    class CommandSystem
+    {
+        public void HandleCommand(byte[] user, byte flag, byte[] cmd)
+        {
+            string[] aryCmd = cmd.ToString().Split(' ');
+
+            switch (aryCmd[0].ToLower())
+            { 
+                case "test":
+                    addchat vbgreen, "Hiya ^_^!";
+                    break;
+            }
+        }
+
+    }
+}
