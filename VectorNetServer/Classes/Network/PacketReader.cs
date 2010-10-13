ca638dd94aecce223f3c414e3ad9d918850c09f7
Lynxy <the_lynxy@hotmail.com>
2010-10-13 12:17:02 -0400

    Added PacketReader class

---
+++ VectorNetServer/Classes/Network/PacketReader.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lynxy.Network
+{
+    public sealed class PacketReader
+    {
+        //TODO: This will assist Packet Parser in reading packets
+    }
+}
