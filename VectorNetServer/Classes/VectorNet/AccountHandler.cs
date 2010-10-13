481a0e23f572382be62499e23f277fe8833cdc8e
AlphaBlend <whizkid3000@hotmail.com>
2010-10-13 16:09:15 -0700

    Added AccountHandler.cs skeleton, to handle accounts from the database

---
+++ VectorNetServer/Classes/VectorNet/AccountHandler.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VNET.Account
+{
+    class AccountHandlerClass
+    {
+        public byte GetAccState(byte[] username)
+        { 
+          // Gets user's rank from database
+
+            // 0x01 = User is ok
+            // 0x02 = User is banned
+        }
+    }
+}
