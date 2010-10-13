fa72a6d9f548f27b9ce6aed56186e2d183ac3711
AlphaBlend <whizkid3000@hotmail.com>
2010-10-13 16:07:59 -0700

    Created RichTextBox.cs to handle main form's RickTextBox

---
+++ VectorNetServer/Classes/VectorNet/RichTextBox.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VNET.RichText
+{
+    class RichTextClass
+    {
+
+        public void AddChat(string[] args)
+        {
+            
+            //if (args.Length % 2 == 0)
+            //{
+            //    for (int i = 0; i < args.Length; i+=2)
+            //    { 
+            //        
+            //    }
+            // }
+        }
+    }
+}
