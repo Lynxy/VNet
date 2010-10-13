05338d8c2c7b093084c52e412d3e2591275bb7ea
Lynxy <the_lynxy@hotmail.com>
2010-10-13 14:28:11 -0400

    Added PacketWrapper class

---
+++ VectorNetServer/Classes/Network/PacketWrapper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lynxy.Network
+{
+    public class PacketWrapper : Packet
+    {
+        public PacketWrapper(TcpClientWrapper sock)
+        {
+
+        }
+    }
+}
