45dc84dfd03e228d1c9b869983aa8f9d0604421f
AlphaBlend <whizkid3000@hotmail.com>
2010-10-13 23:40:01 -0700

    Added IP pool class. We need to talk about this in depth whenever you get the chance next

---
+++ VectorNetServer/Classes/VectorNet/QueuePool.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VNET.QueuePool
+{
+    class QueuePool
+    {
+        public QueuePool()
+        { 
+            // IP Pool for queue sharing
+        }
+    }
+}
