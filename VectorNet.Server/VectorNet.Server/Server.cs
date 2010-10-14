5ed3aeb3b79db49db14475648e7e249cf35e4629
Lynxy <the_lynxy@hotmail.com>
2010-10-14 15:53:55 -0400

    Added SQLite binaries

--- VectorNet.Server/VectorNet.Server/Server.cs
+++ VectorNet.Server/VectorNet.Server/Server.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 
 using Lynxy.Network;
+//using System.Data.SQLite;
 
 namespace VectorNet.Server
 {
@@ -17,7 +18,17 @@ namespace VectorNet.Server
         }
         public Server()
         {
-            //nothing here yet
+            //using (SQLiteConnection cn = new SQLiteConnection(@"DataSource=mydatabase.db"))
+            //{
+            //    cn.Open();
+            //    using (SQLiteCommand cmd = cn.CreateCommand())
+            //    {
+            //        cmd.CommandText = @"";
+            //        cmd.CommandType = System.Data.CommandType.Text;
+            //        cmd.ExecuteReader();
+            //    }
+            //    cn.Clone();
+            //}
         }
     }
 }
