87099eb72fbbb57c6f99802144c21a8a40c3d65b
Lynxy <the_lynxy@hotmail.com>
2010-10-15 18:03:40 -0400

    Added ChannelFunctions.cs and ChannelPoolFunctions.cs

---
+++ VectorNet.Server/VectorNet.Server/Functions/Channel/ChannelFunctions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Lynxy.Network;
+
+namespace VectorNet.Server
+{
+    public partial class Server
+    {
+        //Channel Functions
+        //This class is for methods that deal with a single channel, not multiple channels
+        
+    }
+}
---
+++ VectorNet.Server/VectorNet.Server/Functions/Channel/ChannelPoolFunctions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Lynxy.Network;
+
+namespace VectorNet.Server
+{
+    public partial class Server
+    {
+        //Channel Pool Functions
+        //This class is for methods that must look at the Channels dictionary, not a single channel
+        
+    }
+}
