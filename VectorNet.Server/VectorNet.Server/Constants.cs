0acbaaabd8b718cf3f592b5d83168a1c1d2e2eef
Lynxy <the_lynxy@hotmail.com>
2010-10-15 12:46:40 -0400

    Added Constants.cs file

---
+++ VectorNet.Server/VectorNet.Server/Constants.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Lynxy.Network;
+
+namespace VectorNet.Server
+{
+    public partial class Server
+    {
+        protected const int VNET_LOGON = 0x01;
+        protected const int VNET_CHATEVENT = 0x02;
+        protected const int VNET_TICTACTOE = 0x03;
+        protected const int VNET_CHANNELLIST = 0x04;
+        protected const int VNET_FEATUREQUERY = 0x05;
+        protected const int VNET_KEEPALIVE = 0x06;
+        protected const int VNET_CHANNELJOIN = 0x07;
+        protected const int VNET_QUEUESHARE = 0x09;
+        protected const int VNET_SERVERCHALLENGE = 0x0A;
+        protected const int VNET_VNETPAD = 0x0B;
+
+        
+    }
+}
