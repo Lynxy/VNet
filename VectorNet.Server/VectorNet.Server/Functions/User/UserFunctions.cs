3105871850edd914efc2aa527cb277a018f430ac
Lynxy <the_lynxy@hotmail.com>
2010-10-15 13:21:50 -0400

    Updated UserFunctions

--- VectorNet.Server/VectorNet.Server/Functions/User/UserFunctions.cs
+++ VectorNet.Server/VectorNet.Server/Functions/User/UserFunctions.cs
@@ -9,6 +9,27 @@ namespace VectorNet.Server
 {
     public partial class Server
     {
-        
+        protected void SendLogonResult(User user, LogonResult result)
+        {
+            //if (result == LogonResult.SUCCESS)
+            //{
+            //}
+            //else
+            //{
+                user.Packet.Clear()
+                    .InsertByte((byte)result)
+                    .InsertByte(0) //challenge byte
+                    .InsertStringNT("server ver")
+                    .InsertStringNT("TestEnv")
+                    .InsertDWord(0) //ping
+                    .InsertByte(0) //rank
+                    .Send(VNET_LOGON);
+            //}
+        }
+
+        protected void SendServerInfo(User user)
+        {
+
+        }
     }
 }
