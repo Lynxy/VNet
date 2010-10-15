9a4cb9d3f5a1aec722b5221b6b1f6b638d14f8ee
Lynxy <the_lynxy@hotmail.com>
2010-10-15 13:22:04 -0400

    Added UserPoolFunctions.cs

---
+++ VectorNet.Server/VectorNet.Server/Functions/User/UserPoolFunctions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Lynxy.Network;
+
+namespace VectorNet.Server
+{
+    public partial class Server
+    {
+        protected User GetUserByName(string username)
+        {
+            IEnumerable<User> ret = from u in Users
+                       where u.Username.ToLower() == username.ToLower()
+                       && u.IsOnline == true
+                       select u;
+            if (ret == null)
+                return null;
+            return ret.First();
+        }
+    }
+}
