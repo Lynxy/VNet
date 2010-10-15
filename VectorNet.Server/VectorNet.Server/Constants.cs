1d1395af9ca2e79d2b0258af1bd207b4a4117ff0
Lynxy <the_lynxy@hotmail.com>
2010-10-15 13:01:50 -0400

    Added LogonResult to Constants

--- VectorNet.Server/VectorNet.Server/Constants.cs
+++ VectorNet.Server/VectorNet.Server/Constants.cs
@@ -9,17 +9,24 @@ namespace VectorNet.Server
 {
     public partial class Server
     {
-        protected const int VNET_LOGON = 0x01;
-        protected const int VNET_CHATEVENT = 0x02;
-        protected const int VNET_TICTACTOE = 0x03;
-        protected const int VNET_CHANNELLIST = 0x04;
-        protected const int VNET_FEATUREQUERY = 0x05;
-        protected const int VNET_KEEPALIVE = 0x06;
-        protected const int VNET_CHANNELJOIN = 0x07;
-        protected const int VNET_QUEUESHARE = 0x09;
+        protected const int VNET_LOGON          = 0x01;
+        protected const int VNET_CHATEVENT      = 0x02;
+        protected const int VNET_TICTACTOE      = 0x03;
+        protected const int VNET_CHANNELLIST    = 0x04;
+        protected const int VNET_FEATUREQUERY   = 0x05;
+        protected const int VNET_KEEPALIVE      = 0x06;
+        protected const int VNET_CHANNELJOIN    = 0x07;
+        protected const int VNET_QUEUESHARE     = 0x09;
         protected const int VNET_SERVERCHALLENGE = 0x0A;
-        protected const int VNET_VNETPAD = 0x0B;
+        protected const int VNET_VNETPAD        = 0x0B;
 
-        
+        protected static class LogonResult
+        {
+            public const int SUCCESS            = 0x00;
+            public const int INVALID_PASSWORD   = 0x01;
+            public const int INVALID_USERNAME   = 0x02;
+            public const int ACCOUNT_IN_USE     = 0x03;
+            public const int INVALID_PROTOCOL   = 0x04;
+        }
     }
 }
