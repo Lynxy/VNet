1a21f7a88e403567c2a5096895b235438409d486
Lynxy <the_lynxy@hotmail.com>
2010-10-20 11:34:47 -0400

    [Commit 2 of 2] Commented out old code

--- VectorNet.Server/VectorNet.Server/Constants.cs
+++ VectorNet.Server/VectorNet.Server/Constants.cs
@@ -77,15 +77,15 @@ namespace VectorNet.Server
             UsersOnServer           = 0x03
         }
 
-        protected struct TempUserStruct
-        {
-            public string Username;
-            public string Channel;
-            public string Client;
-            public List<string> bannedChannel;
-            public short ping;
-            public byte flags;
-            public byte banned;
-        }
+        //protected struct TempUserStruct
+        //{
+        //    public string Username;
+        //    public string Channel;
+        //    public string Client;
+        //    public List<string> bannedChannel;
+        //    public short ping;
+        //    public byte flags;
+        //    public byte banned;
+        //}
     }
 }
--- VectorNet.Server/VectorNet.Server/Sending Methods/VNET_LIST.cs
+++ VectorNet.Server/VectorNet.Server/Sending Methods/VNET_LIST.cs
@@ -84,62 +84,62 @@ namespace VectorNet.Server
 
                     List<Channel> ChannelList = Channels.ToList();
 
-                    TempUserStruct temp;
-                    List<TempUserStruct> lstTemp = new List<TempUserStruct>();
+                    //TempUserStruct temp;
+                    //List<TempUserStruct> lstTemp = new List<TempUserStruct>();
 
-                    foreach (Channel c in ChannelList)
-                    {
-                        foreach (User usr in c.GetCompleteUserList())
-                        {
-                            temp = new TempUserStruct();
+                    //foreach (Channel c in ChannelList)
+                    //{
+                    //    foreach (User usr in c.GetCompleteUserList())
+                    //    {
+                    //        temp = new TempUserStruct();
 
-                            temp.Username = usr.Username;
-                            temp.Channel = usr.Channel.Name;
-                            temp.Client = usr.Client;
-                            temp.ping = (short)usr.Ping;
-                            temp.flags = (byte)usr.Flags;
+                    //        temp.Username = usr.Username;
+                    //        temp.Channel = usr.Channel.Name;
+                    //        temp.Client = usr.Client;
+                    //        temp.ping = (short)usr.Ping;
+                    //        temp.flags = (byte)usr.Flags;
 
-                            if (c.IsUserBanned(usr))
-                            {
-                                temp.banned = 0x01;
+                    //        if (c.IsUserBanned(usr))
+                    //        {
+                    //            temp.banned = 0x01;
 
-                                if ((lstTemp.FindAll(f =>
-                                    f.Username == usr.Username)) != null)
+                    //            if ((lstTemp.FindAll(f =>
+                    //                f.Username == usr.Username)) != null)
 
-                                    lstTemp.Find(f => f.Username == usr.Username).bannedChannel.Add(c.Name);
-                                else
-                                {
-                                    temp.bannedChannel.Add(c.Name);
-                                    lstTemp.Add(temp);
-                                }
-                            }
-                            else
-                            {
-                                temp.banned = 0x00;
-                                lstTemp.Add(temp);
-                            }
-                        }
-                    }
+                    //                lstTemp.Find(f => f.Username == usr.Username).bannedChannel.Add(c.Name);
+                    //            else
+                    //            {
+                    //                temp.bannedChannel.Add(c.Name);
+                    //                lstTemp.Add(temp);
+                    //            }
+                    //        }
+                    //        else
+                    //        {
+                    //            temp.banned = 0x00;
+                    //            lstTemp.Add(temp);
+                    //        }
+                    //    }
+                    //}
 
-                    user.Packet.InsertByte((byte)ListType.UsersOnServer)
-                        .InsertWord((short)lstTemp.Count);
+                    //user.Packet.InsertByte((byte)ListType.UsersOnServer)
+                    //    .InsertWord((short)lstTemp.Count);
 
-                    foreach (TempUserStruct usr in lstTemp)
-                    {
-                        user.Packet.InsertStringNT(usr.Username)
-                                   .InsertStringNT(usr.Client)
-                                   .InsertStringNT(usr.Channel);
+                    //foreach (TempUserStruct usr in lstTemp)
+                    //{
+                    //    user.Packet.InsertStringNT(usr.Username)
+                    //               .InsertStringNT(usr.Client)
+                    //               .InsertStringNT(usr.Channel);
 
-                        if (usr.banned == 0x01)
-                            user.Packet.InsertByte(0x01)
-                                       .InsertStringNT(string.Join(((char)1).ToString(), usr.bannedChannel.ToArray()));
-                        else
-                            user.Packet.InsertByte(0x00);
+                    //    if (usr.banned == 0x01)
+                    //        user.Packet.InsertByte(0x01)
+                    //                   .InsertStringNT(string.Join(((char)1).ToString(), usr.bannedChannel.ToArray()));
+                    //    else
+                    //        user.Packet.InsertByte(0x00);
 
-                        user.Packet.InsertWord((short)usr.ping)
-                                   .InsertByte((byte)usr.flags);
-                    }
-                    user.Packet.Send(VNET_LIST);
+                    //    user.Packet.InsertWord((short)usr.ping)
+                    //               .InsertByte((byte)usr.flags);
+                    //}
+                    //user.Packet.Send(VNET_LIST);
                     break;
             }
         }
