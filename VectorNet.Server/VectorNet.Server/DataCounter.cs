38d1b844e266dc563b7a04912796361c4cbd30be
Lynxy <the_lynxy@hotmail.com>
2010-10-21 19:25:45 -0400

    Added DataCounter.cs

---
+++ VectorNet.Server/VectorNet.Server/DataCounter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Lynxy.Network;
+
+namespace VectorNet.Server
+{
+    //public partial class Server
+    //{
+        static public class DataCounter
+        {
+            static public long bytesSent = 0;
+        }
+    //}
+}
