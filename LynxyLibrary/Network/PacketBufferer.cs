84ec878f64fb00eeb2ae259d0542ea3bdb057138
Lynxy <the_lynxy@hotmail.com>
2010-10-22 13:10:51 -0400

    [LynxyLibrary] Removed thread-locks from PacketBufferer.cs. This seems to make client disconnects much faster. Also added another version of the class (static), it is commented out

--- LynxyLibrary/Network/PacketBufferer.cs
+++ LynxyLibrary/Network/PacketBufferer.cs
@@ -28,24 +28,74 @@ namespace Lynxy.Network
 
         protected void ticker_Elapsed(object obj)
         {
-            lock (_locker)
-            {
+            //lock (_locker)
+            //{
                 if (buffer.Length > 0)
                 {
                     _SendData(ref buffer);
                     Array.Resize(ref buffer, 0);
                 }
-            }
+            //}
         }
 
         public void QueuePacket(ref byte[] packet)
         {
-            lock (_locker)
-            {
+            //lock (_locker)
+            //{
                 int origLen = buffer.Length;
                 Array.Resize(ref buffer, origLen + packet.Length);
                 Array.Copy(packet, 0, buffer, origLen, packet.Length);
-            }
+            //}
         }
     }
+
+    //static public class PacketBufferer
+    //{
+    //    public delegate void SendDataDelegate(object state, byte[] data);
+    //    static private SendDataDelegate _SendData;
+
+    //    static private Dictionary<object, byte[]> buffers;
+    //    static private Timer ticker;
+    //    static private readonly object _locker = new object();
+
+    //    static public void Init(int sendInterval, SendDataDelegate SendData)
+    //    {
+    //        buffers = new Dictionary<object, byte[]>();
+    //        ticker = new Timer(ticker_Elapsed, null, sendInterval, sendInterval);
+    //        _SendData = SendData;
+    //    }
+
+    //    static private void ticker_Elapsed(object obj)
+    //    {
+    //        lock (buffers)
+    //        {
+    //            foreach (object state in buffers.Keys)
+    //            {
+    //                if (buffers[state].Length > 0)
+    //                    _SendData(state, buffers[state]);
+    //            }
+    //            buffers.Clear();
+    //        }
+    //    }
+
+    //    static public void QueuePacket(object state, ref byte[] packet)
+    //    {
+    //        lock (buffers)
+    //        {
+    //            if (buffers.ContainsKey(state))
+    //            {
+    //                byte[] buffer = buffers[state];
+    //                int origLen = buffer.Length;
+    //                Array.Resize(ref buffer, origLen + packet.Length);
+    //                Array.Copy(packet, 0, buffer, origLen, packet.Length);
+    //                buffers[state] = buffer;
+    //            }
+    //            else
+    //            {
+    //                buffers.Add(state, packet);
+    //            }
+    //        }
+    //    }
+    //}
+
 }
