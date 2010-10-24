1151aa49a3007cddbf80c9e471a6fe351fc8a0db
AlphaBlend <whizkid3000@hotmail.com>
2010-10-24 02:01:06 -0700

    Created VNET_APPS.cs, and handled the first two IDs (unhandled, and tic-tac-toe)

---
+++ VectorNet.Server/VectorNet.Server/Sending Methods/VNET_APPS.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Lynxy.Network;
+
+namespace VectorNet.Server
+{
+    public partial class Server
+    {
+        public void HandleAppID(User fromSend, PacketReader reader,  User toSend, AppFlags flag)
+        {
+
+
+
+            toSend.Packet.Clear().InsertByte((byte)flag)
+                                 .InsertStringNT(fromSend.Username);
+            switch (flag)
+            {
+                case AppFlags.Unhandled:
+                    toSend.Packet.Send(VNET_APPS);
+
+                    break;
+                case AppFlags.TicTacToe:
+                    byte tttEventID = reader.ReadByte();
+
+                    if (tttEventID == 0x06)
+                    {
+                        Random num = new Random();
+                        int numRand = num.Next(1);
+
+                        byte result = (byte)(numRand == 0 ? 0x01 : 0x02);
+                        toSend.Packet.InsertByte(tttEventID)
+                                     .InsertByte(result)
+                                     .Send(VNET_APPS);
+                    }
+                    else
+                        toSend.Packet.InsertByte(tttEventID).InsertString(reader.ReadToEnd().ToString());
+
+                    break;
+            }
+        }
+    }
+}
