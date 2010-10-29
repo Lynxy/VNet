076e2f10df66c9b301e130e1d6b28872714e64e6
AlphaBlend <whizkid3000@hotmail.com>
2010-10-29 15:44:50 -0700

    Started the actual handling of VNET_SERVERCHALLENGE

--- VectorNet.Server/VectorNet.Server/Packet Handlers/VNET_SERVERCHALLENGE.cs
+++ VectorNet.Server/VectorNet.Server/Packet Handlers/VNET_SERVERCHALLENGE.cs
@@ -11,6 +11,15 @@ namespace VectorNet.Server
     {
         protected void HandlePacket_VNET_SERVERCHALLENGE(User user, PacketReader reader)
         {
+            string GrabChallenge = reader.ReadStringNT();
+
+            if (IsEmptyChallenge(user.Username, GrabChallenge))
+                // blah
+            else
+                if (!GetChallengeState(user.Username, GrabChallenge)
+                {
+                    DisconnectUser(user, "You have provided an incorrect challenge.");
+                }
         }
     }
 }
