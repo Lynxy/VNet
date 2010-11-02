fc29d44d27aa654fca31e3a5d92eb0c7dd8fa2d9
Lynxy <the_lynxy@hotmail.com>
2010-11-02 14:16:26 -0400

    Moved ServerStats.cs into /Classes/

---
+++ VectorNet.Server/VectorNet.Server/Classes/ServerStats.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Lynxy.Network;
+
+namespace VectorNet.Server
+{
+    static public class ServerStats
+    {
+        static public long bytesSent = 0;
+        static public long bytesRecv = 0;
+        static public long lastBytesSent = 0;
+        static public long lastBytesRecv = 0;
+
+        static public long totalConnections = 0;
+        static public long usersOnline = 0;
+        static public string test = "";
+    }
+}
