42b69f1130078d8a3bac2d88132c44fdb4bd2b65
AlphaBlend <whizkid3000@hotmail.com>
2010-11-04 17:10:39 -0700

    Added QueueSharingData, which will be a class to handle all the queues in queue sharing

---
+++ VectorNet.Server/VectorNet.Server/Classes/QueueSharingData.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VectorNet.Server
+{
+    public partial class Server
+    {
+        public class QueueSharingData
+        {
+            protected List<User> QueueUsers;
+            protected string _channel;
+            protected bool PoolStatus;
+            
+            public string channel { get { return _channel; } }
+
+            public QueueSharingData(string channel)
+            {
+                PoolStatus = true;
+                _channel = channel;
+            }
+
+            public bool ExistsIP(string IP)
+            {
+                User ret = QueueUsers.Find(f => f.IPAddress == IP);
+
+                if (ret != null)
+                    return true;
+
+                return false;
+            }
+
+            public bool ExistsName(string name)
+            {
+                User ret = QueueUsers.Find(f => f.Username == name);
+
+                if (ret != null)
+                    return true;
+
+                return false;
+            }
+
+            public void AddUserToQueue(User user)
+            {
+                QueueUsers.Add(user);
+            }
+        }
+    }
+}
