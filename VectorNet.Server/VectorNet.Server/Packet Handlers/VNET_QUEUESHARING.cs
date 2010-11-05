897b6ba11255cbffa4f675148d5825cc27d4753d
AlphaBlend <whizkid3000@hotmail.com>
2010-11-04 17:12:50 -0700

    Added VNET_QUEUESHARING.cs

---
+++ VectorNet.Server/VectorNet.Server/Packet Handlers/VNET_QUEUESHARING.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Lynxy.Network;
+
+namespace VectorNet.Server
+{
+    public partial class Server
+    {
+        protected void HandleVNET_QUEUESHARING(User user, PacketReader reader)
+        {
+            byte getQueueId = reader.ReadByte();
+
+            switch (getQueueId)
+            { 
+
+            }
+        }
+    }
+}
