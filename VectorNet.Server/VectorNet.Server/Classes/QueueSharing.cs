5eac8a0a6674d53c3699ffa6262493ff194d1161
AlphaBlend <whizkid3000@hotmail.com>
2010-11-04 17:10:14 -0700

    Added QueueSharing.cs, with basic handling

---
+++ VectorNet.Server/VectorNet.Server/Classes/QueueSharing.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using VectorNet.Server;
+
+namespace VectorNet.Server
+{
+    public partial class Server
+    {
+        public class QueueSharing
+        {
+            protected List<QueueSharingData> QSQueues;
+
+            public QueueSharing()
+            {
+                QSQueues = new List<QueueSharingData>();
+            }
+
+            public void AddQueue(User user)
+            {
+                QueueSharingData temp = new QueueSharingData(user.BattleNetChannel);
+
+                temp.AddUserToQueue(user);
+                QSQueues.Add(temp);
+            }
+
+            public void RemQueue()
+            { 
+            
+            }
+        }
+    }
+}
