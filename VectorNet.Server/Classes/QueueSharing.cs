3ade6681b3d9c31509da57df4d85e8d4ca60f93f
AlphaBlend <whizkid3000@hotmail.com>
2010-11-23 12:47:40 -0800

    Updated queue sharing data

--- VectorNet.Server/Classes/QueueSharing.cs
+++ VectorNet.Server/Classes/QueueSharing.cs
@@ -7,28 +7,45 @@ using VectorNet.Server;
 
 namespace VectorNet.Server
 {
+
     public partial class Server
     {
         protected class QueueSharing
         {
             protected List<QueueSharingData> QSQueues;
-
+            
             public QueueSharing()
             {
                 QSQueues = new List<QueueSharingData>();
             }
 
+            /// <summary>
+            /// Adds a queue, or includes a user into that queue
+            /// </summary>
+            /// <param name="user">The user to add to the queue</param>
             public void AddQueue(User user)
             {
-                QueueSharingData temp = new QueueSharingData(user.BattleNetChannel);
+                QueueSharingData ret = QSQueues.Find(f => f.channel == user.BattleNetChannel);
 
-                temp.AddUserToQueue(user);
-                QSQueues.Add(temp);
+                if (ret == null)
+                {
+                    QueueSharingData temp = new QueueSharingData(user.BattleNetChannel);
+                    
+                    temp.AddUserToQueue(user);
+                    QSQueues.Add(temp);
+                }
+                else
+                    ret.AddUserToQueue(user);
             }
 
-            public void RemQueue()
-            { 
-            
+            /// <summary>
+            /// Removes a user from the queue pool
+            /// </summary>
+            /// <param name="user">The user object to remove from the queue</param>
+            public void RemQueue(User user)
+             {
+                QueueSharingData ret = QSQueues.Find(f => f.channel == user.BattleNetChannel);
+                if (ret != null) QSQueues.Remove(ret);
             }
         }
     }
--- VectorNet.Server/Classes/QueueSharingData.cs
+++ VectorNet.Server/Classes/QueueSharingData.cs
@@ -21,19 +21,28 @@ namespace VectorNet.Server
                 _channel = channel;
             }
 
+            /// <summary>
+            /// Checks to see if a particular IP exists in the queue pool
+            /// </summary>
+            /// <param name="IP">Represents an IP address</param>
+            /// <returns>Returns true if an IP matches the one passed in, false otherwise</returns>
             public bool ExistsIP(string IP)
             {
                 User ret = QueueUsers.Find(f => f.IPAddress == IP);
 
                 if (ret != null)
                     return true;
-
                 return false;
             }
 
+            /// <summary>
+            /// Checks to see if a particular user exists in the queue pool
+            /// </summary>
+            /// <param name="name">Name represents the name of a user</param>
+            /// <returns>Returns true if name exists in the queue pool, false otherwise</returns>
             public bool ExistsName(string name)
             {
-                User ret = QueueUsers.Find(f => f.Username == name);
+                User ret = QueueUsers.Find(f => f.Username.ToLower() == name.ToLower());
 
                 if (ret != null)
                     return true;
@@ -41,9 +50,34 @@ namespace VectorNet.Server
                 return false;
             }
 
+            /// <summary>
+            /// Adds a user to this queue pool
+            /// </summary>
+            /// <param name="user">User object to add to the queue</param>
             public void AddUserToQueue(User user)
             {
-                QueueUsers.Add(user);
+                if (!QueueUsers.Contains(user))
+                    QueueUsers.Add(user);
+            }
+
+            /// <summary>
+            /// Removes a user from this queue pool
+            /// </summary>
+            /// <param name="user">User object to remove from the queue</param>
+            public void RemUserFromQueue(User user, bool isIPRequest)
+            {
+                if (isIPRequest)
+                {
+                    List<User> ret = new List<User>();
+                    ret = QueueUsers.FindAll(f => f.IPAddress == user.IPAddress);
+
+                    if (ret.Count > 0)
+                        foreach (User u in ret)
+                            QueueUsers.Remove(u);
+                }
+                else
+                    if (QueueUsers.Contains(user))
+                        QueueUsers.Remove(user);    
             }
         }
     }
